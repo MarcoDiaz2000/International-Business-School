3bfa7a9db4e9554217aa68396ff4dd85f3b73b0e
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-04-18 14:21:47 -0600

    Add second page

---
+++ js/index.js
@@ -0,0 +1,4 @@
+document.querySelector('#navbarSupportedContent button.navbar-toggler').addEventListener('click', function() {
+    document.querySelector('#menu-overlay').style.display = 'block';
+  });
+  
